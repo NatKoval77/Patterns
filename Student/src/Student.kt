0ccad16d36af6f6783dfe8aa045cf2cb335922d7
NatKoval77 <157418354+NatKoval77@users.noreply.github.com>
2024-10-28 01:06:32 +0300

    LR1//Added hash constructor

--- Student/src/Student.kt
+++ Student/src/Student.kt
@@ -10,6 +10,17 @@ class Student(
     constructor(id: Int, surname: String, name: String, patronymic: String):
             this(id, surname, name, patronymic, null, null, null, null)
 
+    constructor(hashSt: HashMap<String, Any?>) : this(
+        (hashSt["id"] as? Int) ?: throw IllegalArgumentException("ID обязателен"),
+        (hashSt["surname"] as? String) ?: throw IllegalArgumentException("Фамилия обязательна"),
+        (hashSt["name"] as? String) ?: throw IllegalArgumentException("Имя обязательно"),
+        (hashSt["patronymic"] as? String) ?: throw IllegalArgumentException("Отчество обязательно"),
+        hashSt["phone"]?.toString(),
+        hashSt["tg"]?.toString(),
+        hashSt["email"]?.toString(),
+        hashSt["git"]?.toString()
+    )
+
     fun write() {
         println(
             "Студент $id:\n" + "ФИО - $surname $name $patronymic; " + "Телеграм - ${tg?: "[не указано]"}; " +

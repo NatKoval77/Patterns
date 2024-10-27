05b7cfd28ca1180a97c8a9a7469c6955a4eb92a8
NatKoval77 <157418354+NatKoval77@users.noreply.github.com>
2024-10-28 01:39:40 +0300

    LR1//Added fun isValidPhone() with using companion obj

--- Student/src/Main.kt
+++ Student/src/Main.kt
@@ -1,5 +1,10 @@
 fun main() {
     val frst = Student(1, "Ralf", "Bobby", "Braun", "@RaBBiT")
     val sec = Student(2, "Ruby", "John", "Milligan", git = "github.com/RuMiJ")
-    val thrd = Student(3, "Tomb", "Raider", "Third", phone = "+78889990000")
+    try {
+        val thrd = Student(3, "Tomb", "Raider", "Third", phone = "+78889990000")
+        val ph = Student(4, "Tommy", "Doggy", "Poppy", email = "TDP@mail.ru",phone = "1234567890")
+    } catch (e: IllegalArgumentException) {
+        println(e.message)
+    }
 }
--- Student/src/Student.kt
+++ Student/src/Student.kt
@@ -2,7 +2,15 @@ class Student(
     var id: Int, var surname: String, var name: String, var patronymic: String, var tg: String? = null,
     var git: String? = null, var email: String? = null, var phone: String? = null
 ) {
+    companion object {
+        fun isValidPhone(phone: String): Boolean {
+            return phone.matches(Regex("^(\\+7[0-9]{10}|8[0-9]{10})\$"))
+        }
+    }
+
     init {
+        if (phone != null && !isValidPhone(phone!!))
+            throw IllegalArgumentException("Неверный формат номера!")
         println("Студент $id был добавлен успешно!")
         write()
     }
@@ -10,15 +18,15 @@ class Student(
     constructor(id: Int, surname: String, name: String, patronymic: String):
             this(id, surname, name, patronymic, null, null, null, null)
 
-    constructor(hashSt: HashMap<String, Any?>) : this(
-        (hashSt["id"] as? Int) ?: throw IllegalArgumentException("ID обязателен"),
-        (hashSt["surname"] as? String) ?: throw IllegalArgumentException("Фамилия обязательна"),
-        (hashSt["name"] as? String) ?: throw IllegalArgumentException("Имя обязательно"),
-        (hashSt["patronymic"] as? String) ?: throw IllegalArgumentException("Отчество обязательно"),
-        hashSt["phone"]?.toString(),
-        hashSt["tg"]?.toString(),
-        hashSt["email"]?.toString(),
-        hashSt["git"]?.toString()
+    constructor(hashStudents: HashMap<String, Any?>) : this(
+        (hashStudents["id"] as? Int) ?: throw IllegalArgumentException("ID обязателен"),
+        (hashStudents["surname"] as? String) ?: throw IllegalArgumentException("Фамилия обязательна"),
+        (hashStudents["name"] as? String) ?: throw IllegalArgumentException("Имя обязательно"),
+        (hashStudents["patronymic"] as? String) ?: throw IllegalArgumentException("Отчество обязательно"),
+        hashStudents["phone"]?.toString(),
+        hashStudents["tg"]?.toString(),
+        hashStudents["email"]?.toString(),
+        hashStudents["git"]?.toString()
     )
 
     fun write() {

c8df80ea6e88e228aaef10acdd9ca9f894286d0f
NatKoval77 <157418354+NatKoval77@users.noreply.github.com>
2024-10-28 00:57:00 +0300

    LR1//Added primary and secondary constructors

--- Student/src/Student.kt
+++ Student/src/Student.kt
@@ -2,6 +2,14 @@ class Student(
     var id: Int, var surname: String, var name: String, var patronymic: String, var tg: String? = null,
     var git: String? = null, var email: String? = null, var phone: String? = null
 ) {
+    init {
+        println("Студент $id был добавлен успешно!")
+        write()
+    }
+
+    constructor(id: Int, surname: String, name: String, patronymic: String):
+            this(id, surname, name, patronymic, null, null, null, null)
+
     fun write() {
         println(
             "Студент $id:\n" + "ФИО - $surname $name $patronymic; " + "Телеграм - ${tg?: "[не указано]"}; " +

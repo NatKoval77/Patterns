c61b7ca23e076171a2415b891fe98276d947dbec
NatKoval77 <157418354+NatKoval77@users.noreply.github.com>
2024-10-28 00:34:44 +0300

    LR1//Added code duplication avoidance

--- Student/src/Student.kt
+++ Student/src/Student.kt
@@ -1,52 +1,12 @@
 class Student(
-    id: Int, surname: String, name: String, patronymic: String, tg: String? = null,
-    git: String? = null, email: String? = null, phone: String? = null
+    var id: Int, var surname: String, var name: String, var patronymic: String, var tg: String? = null,
+    var git: String? = null, var email: String? = null, var phone: String? = null
 ) {
-    var id: Int = id
-        get() = field
-        set(value) {
-            field = value
-        }
-    var surname: String = surname
-        get() = field
-        set(value) {
-            field = value
-        }
-    var name: String = name
-        get() = field
-        set(value) {
-            field = value
-        }
-    var patronymic: String? = patronymic
-        get() = field
-        set(value) {
-            field = value
-        }
-    var phone: String? = phone
-        get() = field
-        set(value) {
-            field = value
-        }
-    var tg: String? = tg
-        get() = field
-        set(value) {
-            field = value
-        }
-    var email: String? = email
-        get() = field
-        set(value) {
-            field = value
-        }
-    var git: String? = git
-        get() = field
-        set(value) {
-            field = value
-        }
-
     fun write() {
         println(
             "Студент $id:\n" + "ФИО - $surname $name $patronymic; " + "Телеграм - ${tg?: "[не указано]"}; " +
                     "Гит - ${git?: "[не указано]"}; " + "Почта - ${email?: "[не указано]"}; " + "Телефон - ${phone?: "[не указано]"}.\n"
         )
     }
+
 }

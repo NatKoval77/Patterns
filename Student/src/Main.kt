fa8aabc3b52027d0caee4dc530f6f69f9f1b2adc
NatKoval77 <157418354+NatKoval77@users.noreply.github.com>
2024-12-16 23:01:17 +0300

    LR3//Added implementation Student_list super class
    and added class diagram

--- Student/src/Main.kt
+++ Student/src/Main.kt
@@ -7,59 +7,55 @@ fun testThird(){
         var pathR = ".//stud.txt"
         var pathW = ".//student.txt"
         val studentListManager = Student_list_txt(pathR)
-        val students = studentListManager.readFromTxt(pathR)
-        var student = studentListManager.findById(2)
-        println("Found student: $student\n")
-        studentListManager.writeToTxt(pathW, students)
-        studentListManager.get_k_n_student_short_list(2,1)
+        println("Found student: ${studentListManager.findById(2)}\n")
+        studentListManager.writeToFile(pathW)
         println("Count: ${studentListManager.get_count()}\n")
         studentListManager.orderStudents()
+        println("Ordered:\n")
         studentListManager.get_k_n_student_short_list(0,3)
         var idRem = 3
         studentListManager.removeById(idRem)
         println("After removing $idRem\n")
         studentListManager.get_k_n_student_short_list(0,3)
         println("Count: ${studentListManager.get_count()}\n")
-        val ad = Student(3, "Tomb", "Raider", "Third",
-            git = "github.com/RuMiJ", phone = "+78889990000")
-        studentListManager.addStudent(ad)
+        studentListManager.addStudent(
+            Student(3, "Tomb", "Raider", "Third",
+            git = "github.com/RuMiJ", phone = "+78889990000"))
+        println("After adding:\n")
+        studentListManager.get_k_n_student_short_list(0,5)
+        studentListManager.replaceStudentById(2,
+            Student(2, "Ruby", "John", "Milligan",
+            git = "github.com/RuMiJ", phone = "81234567890"))
+        println("After replacing:\n")
         studentListManager.get_k_n_student_short_list(0,3)
-        var rep = Student(2, "Ruby", "John", "Milligan",
-            git = "github.com/RuMiJ", phone = "81234567890")
-        studentListManager.replaceStudentById(2, rep)
-        studentListManager.get_k_n_student_short_list(0,3)
+        println("- - - END OF TXT! - - -\n")
 
         pathR = ".//testR.json"
-        val forRead = Students_list_JSON(pathR)
-        forRead.get_k_n_student_short_list(0,3)
-        val stListJson = Students_list_JSON()
+        val stListJson = Students_list_JSON(pathR)
         pathW = ".//testW.json"
-        stListJson.writeToJson(pathW)
+        stListJson.writeToFile(pathW)
         stListJson.get_k_n_student_short_list(0,3)
-        student = stListJson.findById(2)
-        println("Found student: $student\n")
+        println("Found student: ${stListJson.findById(2)}\n")
         idRem = 3
         stListJson.removeById(idRem)
         println("After removing $idRem\n")
         stListJson.get_k_n_student_short_list(0,3)
+        println("- - - END OF JSON! - - -\n")
 
         pathR = ".//read.yaml"
         val stListYaml = Students_list_YAML(pathR)
-        stListYaml.readFromYaml(pathR)
         pathW = ".//write.yaml"
-        student = stListYaml.findById(2)
-        println("Found student: $student\n")
-        idRem = 1
-        stListYaml.removeById(idRem)
-        println("After removing student with ID: $idRem\n")
-        stListYaml.writeToYaml(pathW)
-        stListYaml.get_k_n_student_short_list(0, 5)
+        println("Found student: ${stListYaml.findById(2)}\n")
+        stListYaml.writeToFile(pathW)
         stListYaml.orderStudents()
+        println("Ordered:\n")
         stListYaml.get_k_n_student_short_list(0, 5)
-        rep = Student(2, "Ruby", "John", "Milligan",
-            git = "github.com/RuMiJ", email = "rubymilligan@example.com", phone = "81234567890")
-        stListYaml.replaceStudentById(2, rep)
+        stListYaml.replaceStudentById(2,
+            Student(2, "Ruby", "John", "Milligan",
+            git = "github.com/RuMiJ", email = "rubymilligan@example.com", phone = "81234567890"))
+        println("After replacing:\n")
         stListYaml.get_k_n_student_short_list(0, 5)
+        println("- - - END OF YAML! - - -\n")
     }
     catch (e: Exception) {
         println(e.message)
--- Student/src/Students_list_YAML.kt
+++ Student/src/Students_list_YAML.kt
@@ -4,34 +4,35 @@ import java.io.FileReader
 import java.io.FileWriter
 import java.io.IOException
 
-class Students_list_YAML(private var students: MutableList<Student>) {
+class Students_list_YAML(filePath: String) : Students_list(readFromYaml(filePath)) {
     private val yaml = Yaml()
-    constructor() : this(mutableListOf())
-    constructor(filePath: String) : this(mutableListOf()) {
-        students = readFromYaml(filePath)
+    companion object {
+        fun readFromYaml(path: String): MutableList<Student> {
+            val file = File(path)
+            if (!file.exists() || !file.canRead()) throw IOException("Path is incorrect!")
+            val yaml = Yaml()
+            val reader = FileReader(file)
+            val yamlData = yaml.load(reader) as List<Map<String, Any>>
+            return yamlData.map {
+                Student(
+                    id = (it["id"] as Number).toInt(),
+                    surname = (it["surname"] as String),
+                    name = (it["name"] as String),
+                    patronymic = (it["patronymic"] as String),
+                    tg = it["tg"]?.toString(),
+                    git = it["git"]?.toString(),
+                    email = it["email"]?.toString(),
+                    phone = it["phone"]?.toString()
+                )
+            }.toMutableList()
+        }
     }
     //a
-    fun readFromYaml(path: String): MutableList<Student> {
-        val file = File(path)
-        if (!file.exists() || !file.canRead()) throw IOException("Path is incorrect!")
-        val yaml = Yaml()
-        val reader = FileReader(file)
-        val yamlData = yaml.load(reader) as List<Map<String, Any>>
-        return yamlData.map {
-            Student(
-                id = (it["id"] as Number).toInt(),
-                surname = (it["surname"] as String),
-                name = (it["name"] as String),
-                patronymic = (it["patronymic"] as String),
-                tg = it["tg"]?.toString(),
-                git = it["git"]?.toString(),
-                email = it["email"]?.toString(),
-                phone = it["phone"]?.toString()
-            )
-        }.toMutableList()
+    override fun readFromFile(path: String): MutableList<Student> {
+        return readFromYaml(path)
     }
     //b
-    fun writeToYaml(path: String) {
+    override fun writeToFile(path: String) {
         val file = File(path)
         if (!file.exists() || !file.canRead()) throw IOException("Path is incorrect!")
         if (!file.exists()) file.createNewFile()
@@ -51,46 +52,4 @@ class Students_list_YAML(private var students: MutableList<Student>) {
         yaml.dump(data, writer)
         writer.close()
     }
-    //c
-    fun findById(id: Int): Student {
-        return students.first { it.id == id}
-    }
-    //d
-    // n - начиная с какого элемента k - кол-во элементов
-    fun get_k_n_student_short_list(n: Int, k: Int): Data_list<Student_short> {
-        require(n >= 0) { "Индекс n должен быть больше или равен 0." }
-        require(k > 0) { "Количество k должно быть больше 0." }
-        return Data_list_student_short(students.drop(n).take(k).map { Student_short(it) }.toTypedArray())
-    }
-    //e
-    fun orderStudents() {
-        students = students.sortedWith(compareBy { it.getIn() }).toMutableList()
-    }
-    //f
-    fun addStudent(newSt: Student) {
-        val newId = if (students.isEmpty()) 1 else students.maxOf { it.id } + 1
-        val student = Student(newId, newSt.surname, newSt.name, newSt.patronymic,
-            newSt.tg, newSt.git, newSt.email, newSt.phone)
-        students.add(student)
-        println("Студент с ID ${student.id} добавлен!\n")
-    }
-    //g
-    fun replaceStudentById(id: Int, updSt: Student): Boolean {
-        val i = students.indexOfFirst { it.id == id }
-        if (i != -1) {
-            students[i] = Student(id, updSt.surname, updSt.name, updSt.patronymic,
-                updSt.tg, updSt.git, updSt.email, updSt.phone)
-            println("Студент с ID $id заменен!\n")
-            return true
-        } else {
-            println("Студент с ID $id не найден!")
-            return false
-        }
-    }
-    //h
-    fun removeById(id: Int) {
-        students.removeIf { it.id == id }
-    }
-    //i
-    fun get_count(): Int = students.count()
 }

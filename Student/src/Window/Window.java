f62ab65aefae31823935b299c50db1ffb28faa67
NatKoval77 <157418354+NatKoval77@users.noreply.github.com>
2024-12-21 18:37:07 +0300

    LR6//Added three tabs on window

--- Student/src/Window/Window.java
+++ Student/src/Window/Window.java
@@ -4,9 +4,17 @@ import javax.swing.*;
 
 public class Window {
     public void createWindow() {
-        JFrame frame = new JFrame("My Java Window");
-        frame.setSize(400, 300);
+        JFrame frame = new JFrame("Students");
+        frame.setSize(800, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JTabbedPane tabbedPane = new JTabbedPane();
+        JPanel panel1 = new JPanel();
+        tabbedPane.addTab("Tab 1", panel1);
+        JPanel panel2 = new JPanel();
+        tabbedPane.addTab("Tab 2", panel2);
+        JPanel panel3 = new JPanel();
+        tabbedPane.addTab("Tab 3", panel3);
+        frame.getContentPane().add(tabbedPane);
         frame.setVisible(true);
     }
 }
